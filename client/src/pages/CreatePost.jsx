a1009640a1e083f99dc73443b6d412e1d68906cf
Aadil Matloob <8232aadil@gmail.com>
2023-01-21 22:16:09 +0530

    links fixed

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -22,7 +22,7 @@ function CreatePost() {
     if(form.prompt && form.photo){
       setLoading(true);
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post',
+        const response = await fetch('https://dalle2-clone.onrender.com/api/v1/post',
         {
           method: 'POST',
           headers:{
@@ -58,7 +58,7 @@ function CreatePost() {
     if(form.prompt){
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:8080/api/v1/dalle",
+        const response = await fetch("https://dalle2-clone.onrender.com/api/v1/dalle",
          {
            method:'POST',
            headers:{
